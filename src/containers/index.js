febb494b52bbb31291117efcf1ed1db01ddfedb8
lustig314 <60037706+lustig314@users.noreply.github.com>
2022-01-21 23:23:48 +0300

    start project

---
+++ src/containers/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./styles.module.scss";
+
+const MainContainer = () => {
+  return (
+    <div className={styles.mainContainer}>
+
+    </div>
+  );
+};
+
+export default MainContainer;
